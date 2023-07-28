5be45f8c083fb657f0c7731a43b0b540ceba5569
emracatal <134175723+emracatal@users.noreply.github.com>
2023-07-28 10:31:32 +0300

    promise olmadan card tmm

--- ip/index.js
+++ ip/index.js
@@ -1,45 +1,49 @@
 //axios import buraya gelecek
-
+import axios from "axios";
 var benimIP;
-
 
 // ------------ değiştirmeyin --------------
 // licensed to Ergineer 2022
 require("babel-core/register");
 require("babel-polyfill");
-async function ipAdresimiAl(){
-	await axios({
-		method: 'get',
-		url: 'https://apis.ergineer.com/ipadresim',
-	})
-	.then(function (response) {
-		return response.data
-	})
-	.then(function (a) {
-		benimIP=a
-	});
-}				
+async function ipAdresimiAl() {
+  await axios({
+    method: "get",
+    url: "https://apis.ergineer.com/ipadresim",
+  })
+    .then(function (response) {
+      return response.data;
+    })
+    .then(function (a) {
+      benimIP = a;
+    });
+}
 // ------------ değiştirmeyin --------------
-
 
 /*
 	ADIM 1: axios kullanarak, aşağıdaki URL'ye GET sorgusu atacağız
     (tag içindeki yere kendi ipnizi yazarak URL'yi oluşturun):
-    https://apis.ergineer.com/ipgeoapi/<ipniz>
+    https://apis.ergineer.com/ipgeoapi/ipadresim
 	
 	NOT: Bilgisayarın IP adresini öğrenmek için: https://apis.ergineer.com/ipadresim 
 	ADIM 5'e gelene kadar fonksiyonunuzu test etmek için ip nizi URL'ye manuel olarak ekleyebilirsiniz.
 */
+console.log("BAŞLA");
+const response = axios.get("https://apis.ergineer.com/ipgeoapi/176.88.137.149");
+console.log(response);
+console.log("BİTİR");
 
 /*
 	ADIM 2: Geri döndürülen verileri inceleyin, bu sizin ip bilgileriniz! Bileşen fonksiyonunuzu geliştirmek içindeki bu veri yapısını
 	iyice anlamanız gerekmektedir.
-	
+
+	https://flagsapi.com/TR/flat/64.png
 */
+
 /*
 	ADIM 3: Argümanı sadece 1 nesne kabül eden bir fonksiyon oluşturun.
     DOM metotlarını ve özelliklerini kullanarak, şunları gerçekleştirin:
-	NOT: API'den gelen bayrak url'i çalışmazsa alternatif olarak: https://flagsapi.com/
+	NOT: API'den gelen bayrak url'i çalışmazsa alternatif olarak: https://flagsapi.com/ https://flagsapi.com/TR/flat/64.png
 	<div class="card">
 	<img src={ülke bayrağı url} />
 	<div class="card-info">
@@ -53,7 +57,21 @@ async function ipAdresimiAl(){
 	</div>
     </div>
 */
+/* function cardCreate(dizi) {
+  const cardDiv = document.createElement("div");
+  cardDiv.setAttribute("class", "card");
 
+  const flagImg = document.createElement("img");
+  flagImg.setAttribute("src", "https://flagsapi.com/TR/flat/64.png");
+  
+  const cardInfoDiv = document.createElement("div");
+  cardInfoDiv.setAttribute("class", "card-info");
+
+  const 
+
+
+}
+cardCreate(response.data); */
 /*
 	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve 
 	bu kartı DOM olarak .cards elementinin içine ekleyin. 
@@ -67,6 +85,23 @@ async function ipAdresimiAl(){
 	Örnek dinamik URL kullanımı: var url = "https://apis.ergineer.com/ipgeoapi/"+benimIP; 
 */
 
-
-
 //kodlar buraya gelecek
+
+const myCard = (dataDizi) => {
+  const cardInfo = `<div class="card">
+	<img src="https://flagsapi.com/TR/flat/64.png" />
+	<div class="card-info">
+		<h3 class="ip">${dataDizi["sorgu"]}</h3>
+		<p class="ulke">${dataDizi["ülke"]}${dataDizi["ülkeKodu"]}</p>
+		<p>Enlem: ${dataDizi["enlem"]} Boylam: ${dataDizi["boylam"]}</p>
+		<p>Şehir: ${dataDizi["şehir"]}</p>
+		<p>Saat dilimi: ${dataDizi["saatdilimi"]}</p>
+		<p>Para birimi: ${dataDizi["parabirimi"]}</p>
+		<p>ISP: ${dataDizi["isp"]}</p>
+	</div>
+    </div>
+`;
+  return cardInfo;
+};
+
+document.querySelector(".cards").innerHTML = myCard(response.data);
